a513b2fa007b781b439f2ff63bb46fc396019aa7
Ian4634 <developerian0305@gmail.com>
2025-10-27 02:15:25 +0800

    finish home body

--- src/components/DisplayItem/DisplayItem.jsx
+++ src/components/DisplayItem/DisplayItem.jsx
@@ -4,8 +4,9 @@ function DisplayItem({language, imgSrc, title}){
     return (
         <div className="display-item">
             <div className="display-image-container">
-                <div className="title">{title}</div>
+                
                 <img className="display-image" src={imgSrc} alt={imgSrc} />
+                <div className="title">{title}</div>
             </div>
             
         </div>
--- src/pages/home/HomeBody.jsx
+++ src/pages/home/HomeBody.jsx
@@ -4,6 +4,15 @@ import DisplayItem from '../../components/DisplayItem/DisplayItem';
 function HomeBody({language}){
     return (
         <div className="home-body-div">
+            <div className="middle-section-container">
+                <div className="provided-service-container">
+                    <span className="provided-service"><span className="work">work</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A collection of </span>
+                    <span className="provided-service">services I provide</span>
+                </div>
+                
+                <span className="service-introduction">I have worked in all industries and contributed to participated works.</span>
+            </div>
+
             <div className="display-section-container">
                 <DisplayItem language={language} title="Snowboard Instructor" imgSrc="/images/snowboarding.JPG"/>
                 <DisplayItem language={language} title="Surf Instructor" imgSrc="/images/surfing.JPG"/>
